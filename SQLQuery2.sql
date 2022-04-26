7769bbd8fd59a2f927f57308cae34b403b19c9f3
Snehal1499 <snehalpatil1718@gmail.com>
2022-04-26 22:54:53 +0530

    UC2 Created Table

---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+create database payroll_service
+Go
+use payroll_service
+Go
+Create Table employee_payroll
+( Id int Identity primary Key,
+  Name varchar(50) Not Null,
+  Salary money default 1000,
+  StartDate datetime default GetDate()
+) 
+
+select *
+from employee_payroll
+Go
